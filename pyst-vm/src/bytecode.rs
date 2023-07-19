7d1955ff85513781325075354e92ae340c55735c
CanftIn <wwc7033@gmail.com>
2023-07-19 18:35:26 +0800

    add bytecode of vm

---
+++ pyst-vm/src/bytecode.rs
@@ -0,0 +1,179 @@
+extern crate pyst_parser;
+
+use self::pyst_parser::ast;
+use std::collections::HashMap;
+use std::fmt;
+
+#[derive(Clone)]
+pub struct CodeObject {
+  pub instructions: Vec<Instruction>,
+  pub label_map: HashMap<Label, usize>,
+  pub locations: Vec<ast::Location>,
+  pub arg_names: Vec<String>,
+}
+
+impl CodeObject {
+  pub fn new(arg_names: Vec<String>) -> CodeObject {
+    CodeObject {
+      instructions: Vec::new(),
+      label_map: HashMap::new(),
+      locations: Vec::new(),
+      arg_names: arg_names,
+    }
+  }
+}
+
+pub type Label = usize;
+
+#[derive(Debug, Clone)]
+pub enum Instruction {
+  Import {
+    name: String,
+    symbol: Option<String>,
+  },
+  LoadName {
+    name: String,
+  },
+  StoreName {
+    name: String,
+  },
+  StoreSubscript,
+  StoreAttr {
+    name: String,
+  },
+  LoadConst {
+    value: Constant,
+  },
+  UnaryOperation {
+    op: UnaryOperator,
+  },
+  BinaryOperation {
+    op: BinaryOperator,
+  },
+  LoadAttr {
+    name: String,
+  },
+  CompareOperation {
+    op: ComparisonOperator,
+  },
+  Pop,
+  Rotate {
+    amount: usize,
+  },
+  Duplicate,
+  GetIter,
+  Pass,
+  Continue,
+  Break,
+  Jump {
+    target: Label,
+  },
+  JumpIf {
+    target: Label,
+  },
+  MakeFunction,
+  CallFunction {
+    count: usize,
+  },
+  ForIter,
+  ReturnValue,
+  SetupLoop {
+    start: Label,
+    end: Label,
+  },
+  SetupExcept {
+    handler: Label,
+  },
+  PopBlock,
+  Raise {
+    argc: usize,
+  },
+  BuildTuple {
+    size: usize,
+  },
+  BuildList {
+    size: usize,
+  },
+  BuildMap {
+    size: usize,
+  },
+  BuildSlice {
+    size: usize,
+  },
+  PrintExpr,
+  LoadBuildClass,
+  StoreLocals,
+}
+
+#[derive(Debug, Clone)]
+pub enum Constant {
+  Integer { value: i32 }, // TODO: replace by arbitrary big int math.
+  Float { value: f64 },
+  Boolean { value: bool },
+  String { value: String },
+  Code { code: CodeObject },
+  None,
+}
+
+#[derive(Debug, Clone)]
+pub enum ComparisonOperator {
+  Greater,
+  GreaterOrEqual,
+  Less,
+  LessOrEqual,
+  Equal,
+  NotEqual,
+  In,
+  NotIn,
+  Is,
+  IsNot,
+}
+
+#[derive(Debug, Clone)]
+pub enum BinaryOperator {
+  Power,
+  Multiply,
+  MatrixMultiply,
+  Divide,
+  FloorDivide,
+  Modulo,
+  Add,
+  Subtract,
+  Subscript,
+  Lshift,
+  Rshift,
+  And,
+  Xor,
+  Or,
+}
+
+#[derive(Debug, Clone)]
+pub enum UnaryOperator {
+  Not,
+  Invert,
+  Minus,
+  Plus,
+}
+
+/*
+Maintain a stack of blocks on the VM.
+pub enum BlockType {
+    Loop,
+    Except,
+}
+*/
+
+impl fmt::Debug for CodeObject {
+  fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+    let inst_str = self
+      .instructions
+      .iter()
+      .zip(self.locations.iter())
+      .enumerate()
+      .map(|(i, inst)| format!("Inst {}: {:?}", i, inst))
+      .collect::<Vec<_>>()
+      .join("\n");
+    let labelmap_str = format!("label_map: {:?}", self.label_map);
+    write!(f, "Code Object {{ \n{}\n{} }}", inst_str, labelmap_str)
+  }
+}
